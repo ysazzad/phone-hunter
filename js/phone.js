acc0a0d0db3068b38dfcbc16be9343c62a45d1c2
sazzad <hossensazzad1201@gmail.com>
2022-03-01 00:59:05 +0600

    single phone display

--- js/phone.js
+++ js/phone.js
@@ -8,7 +8,7 @@ const searchPhone = () => {
 const loadPhone = (phones) => {
     const phoneDetails = document.getElementById("phone-details")
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         const div = document.createElement("div")
         div.classList.add("col")
         div.innerHTML = `
@@ -17,10 +17,36 @@ const loadPhone = (phones) => {
         <div class="card-body">
             <h5 class="card-title text-center">${phone.phone_name}</h5>
             <p class="card-text text-center">${phone.brand}</p>
-            <button class="btn btn-success text-center"> Explore</button>
+            <button onclick="loadSinglePhone('${phone.slug}')" class="btn btn-success text-center"> Explore</button>
         </div>
     </div>
         `
         phoneDetails.appendChild(div)
     })
+}
+
+const loadSinglePhone = (phoneSlug) => {
+    // console.log(phoneSlug);
+    fetch(`https://openapi.programming-hero.com/api/phone/${phoneSlug}`)
+        .then(response => response.json())
+        .then(data => displaySinglePhone(data.data))
+}
+const displaySinglePhone = (info) => {
+    console.log(info);
+    const singlePhone = document.getElementById("singlePhone")
+    const div = document.createElement("div")
+    div.innerHTML = `
+    <div class="card">
+       <img class="w-50 mx-auto" src="${info.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title text-center">${info.brand}</h5>
+        <p class="card-text text-center">${info.releaseDate ? info.releaseDate : 'no release date found'}</p>
+        <p class="card-text text-center"> ${info.mainFeatures.storage}</p>
+        <p class="card-text text-center"> ${info.mainFeatures.displaySize}</p>
+        <p class="card-text text-center"> ${info.mainFeatures.chipSet}</p>
+        <p class="card-text text-center"> ${info.mainFeatures.memory}</p>
+    </div>
+    </div>
+    `
+    singlePhone.appendChild(div)
 }
