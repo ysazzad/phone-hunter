aeb183c1ba89e9bd2fa86ea54c4934ff7b3ef9a7
sazzad <hossensazzad1201@gmail.com>
2022-03-01 15:39:55 +0600

    error handling

--- js/phone.js
+++ js/phone.js
@@ -8,8 +8,23 @@ const searchPhone = () => {
 const loadPhone = (phones) => {
     // console.log(phones);
     let phones20 = phones.slice(0, 20)
+    if (phones20.length === 0) {
+        const phoneDetails = document.getElementById("phone-details")
+        phoneDetails.innerHTML = ''
+        const errorMsg = document.getElementById("error-message")
+        const div = document.createElement("div")
+        div.innerHTML = `
+        <div class="alert alert-danger text-center w-50 mx-auto" role="alert">
+          No Phone Found
+        </div>
+        `
+        errorMsg.appendChild(div)
+        return
+    }
     console.log(phones20);
     const phoneDetails = document.getElementById("phone-details")
+    const errorMsg = document.getElementById("error-message")
+    errorMsg.innerHTML = ''
     phones20.forEach(phone => {
         console.log(phone);
         const div = document.createElement("div")
@@ -35,12 +50,13 @@ const loadSinglePhone = (phoneSlug) => {
         .then(data => displaySinglePhone(data.data))
 }
 const displaySinglePhone = (info) => {
-    console.log(info.others);
+
+    // console.log(info.others);
     // console.log(info.mainFeatures.sensors);
     const singlePhone = document.getElementById("singlePhone")
+    singlePhone.innerHTML = ''
     // const singlePhone = document.getElementById("singlePhone")
     const div = document.createElement("div")
-
     div.innerHTML = `
     <div class="card">
        <img class="w-25 mx-auto" src="${info.image}" class="card-img-top" alt="...">
@@ -60,22 +76,21 @@ const displaySinglePhone = (info) => {
         // console.log(data);
         const div = document.createElement("div")
         div.innerHTML = `
-        
         <p> ${data} </p>
         `
         singlePhone.appendChild(div)
-
     })
-    const othersInfo = info.others
+    const othersInfo = info.others ? info.others : 'no result found'
+    // console.log(othersInfo);
     const ul = document.createElement("ul")
     ul.innerHTML = `
     <h4> Others Info</h4>
-    <p> ${othersInfo.WLAN}</p>
-    <p> ${othersInfo.Bluetooth}</p>
-    <p> ${othersInfo.GPS}</p>
-    <p> ${othersInfo.NFC}</p>
-    <p> ${othersInfo.Radio}</p>
-    <p> ${othersInfo.USB}</p>
+    <p> ${othersInfo.WLAN ? othersInfo.WLAN : 'no result found'}</p>
+    <p> ${othersInfo.Bluetooth ? othersInfo.Bluetooth : 'no result found'}</p>
+    <p> ${othersInfo.GPS ? othersInfo.GPS : 'no result found'}</p>
+    <p> ${othersInfo.NFC ? othersInfo.NFC : 'no result found'}</p>
+    <p> ${othersInfo.Radio ? othersInfo.Radio : 'no result found'}</p>
+    <p> ${othersInfo.USB ? othersInfo.USB : 'no result found'}</p>
     `
     singlePhone.appendChild(ul)
 
