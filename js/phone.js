1c78f06d7b55d6ead07ab57c06a4fcb581fd419c
sazzad <hossensazzad1201@gmail.com>
2022-03-01 12:19:22 +0600

    bonus part added

--- js/phone.js
+++ js/phone.js
@@ -6,9 +6,12 @@ const searchPhone = () => {
 }
 
 const loadPhone = (phones) => {
+    // console.log(phones);
+    let phones20 = phones.slice(0, 20)
+    console.log(phones20);
     const phoneDetails = document.getElementById("phone-details")
-    phones.forEach(phone => {
-        // console.log(phone);
+    phones20.forEach(phone => {
+        console.log(phone);
         const div = document.createElement("div")
         div.classList.add("col")
         div.innerHTML = `
@@ -26,18 +29,21 @@ const loadPhone = (phones) => {
 }
 
 const loadSinglePhone = (phoneSlug) => {
-    // console.log(phoneSlug);
+    console.log(phoneSlug);
     fetch(`https://openapi.programming-hero.com/api/phone/${phoneSlug}`)
         .then(response => response.json())
         .then(data => displaySinglePhone(data.data))
 }
 const displaySinglePhone = (info) => {
-    console.log(info);
+    console.log(info.others);
+    // console.log(info.mainFeatures.sensors);
     const singlePhone = document.getElementById("singlePhone")
+    // const singlePhone = document.getElementById("singlePhone")
     const div = document.createElement("div")
+
     div.innerHTML = `
     <div class="card">
-       <img class="w-50 mx-auto" src="${info.image}" class="card-img-top" alt="...">
+       <img class="w-25 mx-auto" src="${info.image}" class="card-img-top" alt="...">
     <div class="card-body">
         <h5 class="card-title text-center">${info.brand}</h5>
         <p class="card-text text-center">${info.releaseDate ? info.releaseDate : 'no release date found'}</p>
@@ -49,4 +55,29 @@ const displaySinglePhone = (info) => {
     </div>
     `
     singlePhone.appendChild(div)
+    const sensors = info.mainFeatures.sensors
+    sensors.forEach(data => {
+        // console.log(data);
+        const div = document.createElement("div")
+        div.innerHTML = `
+        
+        <p> ${data} </p>
+        `
+        singlePhone.appendChild(div)
+
+    })
+    const othersInfo = info.others
+    const ul = document.createElement("ul")
+    ul.innerHTML = `
+    <h4> Others Info</h4>
+    <p> ${othersInfo.WLAN}</p>
+    <p> ${othersInfo.Bluetooth}</p>
+    <p> ${othersInfo.GPS}</p>
+    <p> ${othersInfo.NFC}</p>
+    <p> ${othersInfo.Radio}</p>
+    <p> ${othersInfo.USB}</p>
+    `
+    singlePhone.appendChild(ul)
+
 }
+
